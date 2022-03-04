651ff4b6564bc6f269f428aadd7372c3284c0b9c
EunSeop Shin <kairos9603@gmail.com>
2022-03-04 23:07:35 +0900

    net.ipv4.conf.all.rp_filter = 1 추가

--- install-master.sh
+++ install-master.sh
@@ -56,6 +56,7 @@ cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
 net.bridge.bridge-nf-call-iptables = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward = 1
+net.ipv4.conf.all.rp_filter = 1
 EOF
 
 sudo sysctl --system
